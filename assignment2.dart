8dc0e60f137be6deb0d9571d8ae59ab48a1f789e
sakai-aigga <sajinghimire2010@gmail.com>
2025-12-06 11:32:40 +0545

    Completed Assignment 2

---
+++ assignment2.dart
@@ -0,0 +1,35 @@
+class studentDetails {
+  String name;
+  double mark1, mark2, mark3, total = 0.00, avg = 0.00;
+
+  studentDetails(this.name, this.mark1, this.mark2, this.mark3);
+
+  void calculateTotalAndAverage() {
+    total = mark1 + mark2 + mark3;
+    avg = total / 3;
+    print('Total marks obtained by $name is: $total');
+    print('Average marks obtained by $name is $avg');
+  }
+
+  void DisplayGrade() {
+    if (avg >= 80) {
+      print("You obtained Grade A");
+    } else if (avg >= 60) {
+      print("You obtained Grade B");
+    } else if (avg >= 40) {
+      print("You obtained Grade C");
+    } else {
+      print("Failed!!! Grade F");
+    }
+  }
+}
+
+void main() {
+  studentDetails s1 = studentDetails("Ram", 80, 90, 70);
+  s1.calculateTotalAndAverage();
+  s1.DisplayGrade();
+  print("\n");
+  studentDetails s2 = studentDetails("Shyam", 60, 50, 40);
+  s2.calculateTotalAndAverage();
+  s2.DisplayGrade();
+}
