cbf8e886a38bea1dd5633e2630a79ad09f3f0bd1
sakai-aigga <sajinghimire2010@gmail.com>
2025-12-06 11:31:48 +0545

    Completed Task 1

---
+++ task.dart
@@ -0,0 +1,31 @@
+void main() {
+  //task 1
+  /*int age = 20;
+  String name = 'Sajin';
+  String dept = 'STEAM';
+  print(
+    "Your name is: $name You are $age years old and belong to $dept department.",
+  );
+  //task 2
+  print("Enter any two numbers:");
+  int num1 = int.parse(stdin.readLineSync()!);
+  int num2 = int.parse(stdin.readLineSync()!);
+  int sum = num1 + num2;
+  print("The sum of $num1 and $num2 is: $sum");
+  
+  //task 3
+  print("Enter your marks:");
+  int marks = int.parse(stdin.readLineSync()!);
+  if (marks >= 40) {
+    print("Pass!");
+  } else {
+    print("fail!");
+  }
+  */
+
+  //task 4
+  int i;
+  for (i = 1; i <= 10; i++) {
+    print(i);
+  }
+}
