05d5ebb7533ebde8be6e9bed4629494f7f6d9930
saZin <sajinghimire2010@gmail.com>
2025-12-06 12:32:21 +0545

    Added comments
    Added comments for clarity and initialized variables.

--- assignment2.dart
+++ assignment2.dart
@@ -1,16 +1,17 @@
+// Creating a Class
 class studentDetails {
   String name;
   double mark1, mark2, mark3, total = 0.00, avg = 0.00;
 
-  studentDetails(this.name, this.mark1, this.mark2, this.mark3);
-
+  studentDetails(this.name, this.mark1, this.mark2, this.mark3); //Initialized Variables
+//Function for average calculation
   void calculateTotalAndAverage() {
     total = mark1 + mark2 + mark3;
     avg = total / 3;
     print('Total marks obtained by $name is: $total');
     print('Average marks obtained by $name is $avg');
   }
-
+//Function to display Grade according to the average marks obtained
   void DisplayGrade() {
     if (avg >= 80) {
       print("You obtained Grade A");
@@ -23,12 +24,14 @@ class studentDetails {
     }
   }
 }
-
+//Main Function
 void main() {
+  //Object S1 of the sutdentDetails Class
   studentDetails s1 = studentDetails("Ram", 80, 90, 70);
   s1.calculateTotalAndAverage();
   s1.DisplayGrade();
   print("\n");
+  //Object S2 of the sutdentDetails Class
   studentDetails s2 = studentDetails("Shyam", 60, 50, 40);
   s2.calculateTotalAndAverage();
   s2.DisplayGrade();
